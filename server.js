d38abee34dcc8446cbe0f877d21a6f6e675ec239
Roshan Kanwar <roshan.18je0696@am.iitism.ac.in>
2020-05-17 17:11:08 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const bcrypt = require('bcrypt-nodejs');
+const knex = require('knex')
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : '0708',
+    database : 'smart-brain'
+  }
+});
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/",(req,res)=>{ res.send(database.users) })
+
+app.post("/signin",signin.handleSignin(db ,bcrypt))
+
+app.post("/register",register.handleRegister(db, bcrypt))
+
+app.get("/profile/:id",profile.handleProfile(db, bcrypt))
+
+app.put("/image", image.handleImage(db))
+
+app.post('/imageurl', (req,res) => { image.handleApiCall(req,res) })
+
+app.listen(3000,() => {
+	console.log("Server started at port 3000.");
+});
